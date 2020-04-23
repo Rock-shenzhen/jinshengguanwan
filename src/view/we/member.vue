1f8547dbc310b59aedfb5ba588791835afb87f99
zhuminghui <875939853@qq.com>
2020-04-23 22:46:57 +0800

    修改部分子页面

---
+++ src/view/we/member.vue
@@ -0,0 +1,331 @@
+<template>
+ <div>
+  <div class="wrap_con">
+	<div class="sub_main_con cf pb_fre">
+		<div class="sub_menu fnt_14">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<div class="sub_menu fnt_14">
+			<a href="http://www.jxcc.com:80/">首页</a>
+			<span></span>
+				    <a href="http://www.jxcc.com:80/channel/4a864176f35f450689df46cbe6916cfb.html">我们</a>
+				    	<span></span>
+				    <a href="http://www.jxcc.com:80/channel/fbc9bc1708e44538a53dcb44d7b06ebb.html">江铜人</a>
+		</div>		</div>
+		<div class="sub_content cf mt_55">
+					<h1 class="gs_title fnt_32">江铜人</h1>
+					<div class="peo_01 mt_70">
+						<ul class="mt_45">
+									<li class="li01">
+										<h1 class="fnt_24">同心</h1>
+										<p class="fnt_12 mt_27">
+											是江铜人团结向上主流精神的体现。<br>同心，是上下同欲、和谐包容，是我们对江铜事业的共识。
+										</p>
+									</li>
+									<li class="li01">
+										<h1 class="fnt_24">同创</h1>
+										<p class="fnt_12 mt_27">
+											是同心的延展。江铜人应以创造价值、创造业绩为导向，实现超越和突破；<br>进一步创新文化、创新机制、创新管理、创新技术，提升企业竞争力。
+										</p>
+									</li>
+									<li class="li01">
+										<h1 class="fnt_24">同进</h1>
+										<p class="fnt_12 mt_27">
+											是“同心同创”的必然要求。“同进”既强调不同业务领域的同频共振、<br>同步发展；又倡导员工与企业、企业与社会的共同进步。
+										</p>
+									</li>
+						</ul>
+					</div>
+					<div class="peo_02 mt_70">
+						<ul class="mt_45">
+										<li>
+												<span class="peo2_pic">
+													<img src="/uploadFiles/uploadImgs/20180130/e3b806b9e58d48b0a52fd9082021e968.jpg">
+												</span>
+												<span class="peo2_conx fnt_12 mt_10">
+													<h1 class="fnt_18">高思原</h1>
+													<h2 class="fnt_14">贵溪冶炼厂电解车间</h2>
+												</span>
+										</li>
+										<li>
+												<span class="peo2_pic">
+													<img src="/uploadFiles/uploadImgs/20180130/fad9201d4b744f20a979805f0061867d.jpg">
+												</span>
+												<span class="peo2_conx fnt_12 mt_10">
+													<h1 class="fnt_18">江小东（彝族）</h1>
+													<h2 class="fnt_14">四川江铜稀土有限责任公司</h2>
+												</span>
+										</li>
+										<li>
+												<span class="peo2_pic">
+													<img src="/uploadFiles/uploadImgs/20180130/2afab659b2964b9d93942e1da9520c68.jpg">
+												</span>
+												<span class="peo2_conx fnt_12 mt_10">
+													<h1 class="fnt_18">魏明</h1>
+													<h2 class="fnt_14">城门山铜矿采矿场</h2>
+												</span>
+										</li>
+										<li>
+												<span class="peo2_pic">
+													<img src="/uploadFiles/uploadImgs/20180130/2827d390dd2040b18b8178d4ce6fda0e.jpg">
+												</span>
+												<span class="peo2_conx fnt_12 mt_10">
+													<h1 class="fnt_18">汪洋</h1>
+													<h2 class="fnt_14">永平铜矿选矿厂</h2>
+												</span>
+										</li>
+						</ul>
+					</div>
+					<div class="peo_03 mt_30">
+						<ul class="mt_45">
+										<li class="mb_20">
+											<span class="peo_le"><img src="/uploadFiles/uploadImgs/20180315/c779a5c8d8da4aa8a79bf4d0a1e27817.png"></span>
+											<span class="peo_ri fnt_12 mt_85">
+												<h1 class="fnt_24">谢建辉</h1>
+												<h2 class="fnt_14">全国五一劳动奖章、第十三届全国人大代表</h2>
+												<p class="fnt_14 mt_30">与电气设备结缘28年，我坚信“博观而约取，厚积而薄发”。期间，我陆续获得了众多荣誉，更光荣当选第十三届全国人大代表。我感觉，江铜是一片成长的热土，身为其中的一员，我们应该践行使命,履职担当,与企业共成长。</p>
+											</span>
+										</li>
+										<li class="mb_20">
+											<span class="peo_le"><img src="/uploadFiles/uploadImgs/20180315/5708a2674b3e4e7e86b6da825dedde64.png"></span>
+											<span class="peo_ri fnt_12 mt_85">
+												<h1 class="fnt_24">雷寅</h1>
+												<h2 class="fnt_14">江西省劳动模范</h2>
+												<p class="fnt_14 mt_30">工作三十几年，从一名普通员工到省劳模，我伴随着江铜的发展壮大而成长、成熟；这一路走来，从我所学专业，干我所学专长，能为公司尽自己的一份力量，我感到非常的自豪。江铜的“工匠精神”必将在我们身上延续！</p>
+											</span>
+										</li>
+						</ul>
+					</div>
+					</div>
+	</div>
+</div>
+ </div>
+</template>
+<script>
+export default {
+ 
+}
+</script>
+<style lang="">
+ .wrap_con {
+    margin: 0 auto;
+    background: url(../img/zz_bac.jpg) no-repeat bottom center;
+    width: 100%;
+}
+.pb_fre {
+    padding-bottom: 0px!important;
+}
+.sub_main_con {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 38px 0 0;
+}
+.sub_menu {
+    float: left;
+}
+.fnt_14 {
+    font-size: 14px;
+}
+.sub_menu a {
+    float: left;
+    color: #999999;
+    margin-right: 10px;
+}
+.sub_menu span {
+    float: left;
+    margin-right: 10px;
+    background: url(../img/sub_m_b.png) no-repeat 0 1px;
+    width: 10px;
+    height: 18px;
+}
+.sub_menu a {
+    float: left;
+    color: #999999;
+    margin-right: 10px;
+}
+.pb_fre .sub_content {
+    padding-bottom: 65px;
+}
+.sub_content {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    padding-bottom: 100px;
+}
+
+.mt_55 {
+    margin-top: 55px;
+}
+.gs_title {
+    text-align: center;
+}
+.fnt_32 {
+    font-size: 32px;
+}
+.peo_01 {
+    float: left;
+    width: 99.9%;
+    min-height: 100px;
+    padding: 4% 0 4%;
+    border: 1px solid #dbc07f;
+}
+
+.mt_70 {
+    margin-top: 70px;
+}
+.peo_01 .mt_45 {
+    margin-top: 0;
+}
+.peo_01 ul {
+    height: 80%;
+}
+.mt_45 {
+    margin-top: 45px;
+}
+.peo_01 li:first-child, .peo_01 li:last-child {
+    border-right: none;
+    border-left: none;
+}
+
+.peo_01 li {
+    float: left;
+    padding: 0 1% 0 1%;
+    width: 31%;
+    border-right: 1px solid #dbc07f;
+    border-left: 1px solid #dbc07f;
+}
+.peo_01 li h1 {
+    color: #d6a120;
+}
+.fnt_24 {
+    font-size: 24px;
+}
+h1, h2, h3, h4, h5, h6 {
+    font-size: 100%;
+    font-weight: normal;
+}
+.peo_01 li p {
+    color: #868080;
+    line-height: 22px;
+}
+.fnt_12 {
+    font-size: 12px;
+}
+.mt_27 {
+    margin-top: 27px;
+}
+.peo_02.mt_70 {
+    margin-top: 35px;
+}
+
+.peo_02 {
+    float: left;
+}
+.mt_45 {
+    margin-top: 45px;
+}
+.peo_02 li {
+    float: left;
+    width: 23.7%;
+    overflow: hidden;
+    margin-right: 1.73%;
+}
+.peo_02 li span.peo2_pic {
+    padding: 0;
+    width: 100%;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 66.7%;
+}
+
+.peo_02 li span {
+    padding: 5% 5% 5% 5%;
+    width: 100%;
+    float: left;
+    min-height: 62px;
+    box-sizing: border-box;
+}
+.peo_02 li img {
+    width: 100%;
+    -webkit-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+}
+img {
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    -ms-transition: all 0.5s;
+    transition: all 0.5s;
+    border: 0;
+    display: inline-block;
+}
+img {
+    vertical-align: top;
+}
+.peo_02 li span {
+    padding: 5% 5% 5% 5%;
+    width: 100%;
+    float: left;
+    min-height: 62px;
+    box-sizing: border-box;
+}
+
+.fnt_12 {
+    font-size: 12px;
+}
+.mt_10 {
+    margin-top: 10px;
+}
+.peo_02 li h1 {
+    font-weight: bold;
+}
+
+.fnt_18 {
+    font-size: 18px;
+}
+.peo_02 li h2 {
+    margin-top: 5px;
+    color: #666666;
+}
+.fnt_14 {
+    font-size: 14px;
+}
+.peo_03 {
+    float: left;
+}
+.mt_30 {
+    margin-top: 30px;
+}
+.peo_03 li {
+    float: left;
+    width: 100%;
+}
+
+.mb_20 {
+    margin-bottom: 20px;
+}
+.peo_le {
+    float: left;
+    width: 49.2%;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 37%;
+}
+.peo_03 li img {
+    width: 100%;
+    -webkit-transition: all 0.5s;
+    -o-transition: all 0.5s;
+    transition: all 0.5s;
+}
+.peo_ri {
+    float: right;
+    width: 46%;
+}
+.fnt_12 {
+    font-size: 12px;
+}
+
+.mt_85 {
+    margin-top: 85px;
+}
+</style>
