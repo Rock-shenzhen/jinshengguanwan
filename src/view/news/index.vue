2f81c199f5418cd89c728929321c92adecb33716
mh.zhu <875939853@qq.com>
2020-04-23 23:26:24 +0800

    增加子页面

--- src/view/news/index.vue
+++ src/view/news/index.vue
@@ -1,10 +1,329 @@
 <template>
-  <div id="news">新闻</div>
+  <div id="news">
+   <!-- <div class="s_banner">
+      <div class="item">
+        <img src="../../../assets/0417f07f17ca4deb93d3ef262540e341.jpg" alt />
+      </div>
+      <div class="banner_area w1280" style="width: 1280px;">
+        <span class="s_b_text">
+          <h1>传媒视点</h1>
+          <h4></h4>
+        </span>
+      </div>
+      <div class="wrap_con wrap_con_nobac">
+        <div class="sub_main_con cf pb_fre">
+          <div class="sub_menu fnt_14">
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <div class="sub_menu fnt_14">
+              <a href="http://www.jxcc.com:80/">首页</a>
+              <span></span>
+              <a href="http://www.jxcc.com:80/channel/6259e25f77314c4781f0bdd18c60a390.html">传媒视点</a>
+              <span></span>
+              <a href="http://www.jxcc.com:80/channel/6259e25f77314c4781f0bdd18c60a390.html">公司新闻</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wrap_con_nobac pt_50">
+        <div class="sub_news">
+          <div class="news_top mt_20">
+            <a
+              href="http://www.jxcc.com:80/content/10be2e7b619d4027875e1ad4aeea86dc.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+              class="news_l_a"
+              style="position: relative; overflow: hidden;"
+            >
+              <img
+                src="../../../assets/4ab9fd8e91f747cf9cdfd69e1afc7c69.png"
+                class="resize_img"
+                style="position: absolute; top: 0px; left: -116.364px; width: 873px; height: 480px; max-width: none;"
+              />
+            </a>
+            <div class="news_right mt_130">
+              <h1 class="fnt_24">
+                <a
+                  href="http://www.jxcc.com:80/content/10be2e7b619d4027875e1ad4aeea86dc.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+                >一棵小竹笋走出脱贫新路径</a>
+              </h1>
+              <p class="fnt_14 mt_35">一棵小竹笋，如今已带动起双红村50多个贫困家庭就业。</p>
+              <span class="news_top_date fnt_14 mt_35">2020-04-21</span>
+              <a
+                class="news_top_mm fnt_14 mt_40"
+                href="http://www.jxcc.com:80/content/10be2e7b619d4027875e1ad4aeea86dc.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+              >查看详情</a>
+            </div>
+          </div>
+        </div>
+        <div class="sub_main_con cf pb_fre">
+          <div class="sub_content cf mt_55">
+            <ul class="pt_20 news_cell mb_100 mt_15">
+              <li>
+                <span class="news_date mt_45 ml_13">
+                  <span class="news_d_d fnt_24">03/27</span>
+                  <span class="news_d_y fnt_14">2020</span>
+                  <p id="fulltime" style="display:none;" class="time_phone">2020-03-27</p>
+                </span>
+                <h1 class="fnt_20 mt_35 ml_13">
+                  <a
+                    href="http://www.jxcc.com:80/content/a230c0a2171f401f9595b570dff9ccba.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+                  >公司绿色发展再部署：要求2020年国家级创绿达标百分百</a>
+                </h1>
+                <p class="fnt_14 mt_35 ml_13">打造绿色发展样板，是公司“三年创新倍增”的重要内涵。</p>
+                <a
+                  href="http://www.jxcc.com:80/content/a230c0a2171f401f9595b570dff9ccba.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+                  class="news_mm fnt_12 mt_30 ml_13"
+                >更多</a>
+              </li>
+              <li>
+                <span class="news_date mt_45 ml_13">
+                  <span class="news_d_d fnt_24">02/17</span>
+                  <span class="news_d_y fnt_14">2020</span>
+                  <p id="fulltime" style="display:none;" class="time_phone">2020-02-17</p>
+                </span>
+                <h1 class="fnt_20 mt_35 ml_13">
+                  <a
+                    href="http://www.jxcc.com:80/content/96571655d6724191ab3fded11ffedf3d.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+                  >追踪江铜援鄂“逆行者”：与省委书记视频连线、与确诊病人面对面……</a>
+                </h1>
+                <p class="fnt_14 mt_35 ml_13">&#8203;从出发到现在，饶鹏程抵达随州已有6天。这六天都发生了什么……</p>
+                <a
+                  href="http://www.jxcc.com:80/content/96571655d6724191ab3fded11ffedf3d.html?type=2&amp;columnId=6259e25f77314c4781f0bdd18c60a390"
+                  class="news_mm fnt_12 mt_30 ml_13"
+                >更多</a>
+              </li>
+            </ul>
+        
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <div class="page_area cf fnt_16">
+              <a
+                class="page_left page_l_end"
+                title="上一页"
+                href="javascript:void(0);"
+              ></a>
+              <a class="page_num page_on" href="javascript:void(0);">1</a>&nbsp;
+              <a class="page_num" href="javascript:void(0);">2</a>&nbsp;
+              <a class="page_num" href="javascript:void(0);">3</a>&nbsp;
+              <a class="page_num" href="javascript:void(0);">4</a>&nbsp;
+              <a class="page_num" href="javascript:void(0);">5</a>&nbsp;
+              <a class="page_num">...</a>
+              <a class="page_num" href="javascript:void(0);">21</a>&nbsp;
+              <a class="page_right" title="下一页" href="javascript:void(0);"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
-
 <style>
+ .s_banner {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 20.9%;
+}
+.s_banner img {
+  width: 100%;
+}
+img {
+  -webkit-transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  -ms-transition: all 0.5s;
+  transition: all 0.5s;
+  border: 0;
+  display: inline-block;
+}
+.banner_area {
+  position: absolute;
+  max-width: 1280px;
+  left: 50%;
+  bottom: 10%;
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -o-transform: translateX(-50%);
+  -ms-transform: translateX(-50%);
+  transform: translateX(-50%);
+}
+.w1280 {
+  margin: 0 auto;
+  width: 1280px;
+}
+.s_b_text h1,
+.s_b_text h4 {
+  color: #fff;
+}
+.fnt_30 {
+  font-size: 30px;
+}
+.s_b_text {
+  line-height: 33px;
+  text-align: center;
+  float: left;
+  padding-left: 13px;
+  /* margin-top: 22%; */
+}
+.wrap_con_nobac {
+  margin: 0 auto;
+  width: 100%;
+  background: none;
+}
+.wrap_con {
+  margin: 0 auto;
+  background: url(../../../assets/zz_bac.jpg) no-repeat bottom center;
+  width: 100%;
+}
+.pb_fre {
+  padding-bottom: 0px !important;
+}
+.sub_main_con {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 38px 0 0;
+}
+.sub_menu {
+  float: left;
+}
+.fnt_14 {
+  font-size: 14px;
+}
+meta {
+  display: none;
+}
+.sub_menu a {
+  float: left;
+  color: #999999;
+  margin-right: 10px;
+}
+a {
+  color: #333;
+  -webkit-transition: all 0.2s;
+  -moz-transition: all 0.2s;
+  -ms-transition: all 0.2s;
+  transition: all 0.2s;
+}
+.sub_menu span {
+  float: left;
+  margin-right: 10px;
+  background: url(../../../assets/sub_m_b.png) no-repeat 0 1px;
+  width: 10px;
+  height: 18px;
+}
+.wrap_con_nobac {
+  margin: 0 auto;
+  width: 100%;
+  background: none;
+}
+
+.pt_50 {
+  padding-top: 50px;
+}
+.sub_news {
+  float: left;
+  width: 100%;
+  padding-bottom: 60px;
+  /* height: 550px; */
+  background: url(../../../assets/new_t_bac.jpg) repeat-x bottom;
+}
+.news_top {
+  margin: 0 auto;
+  max-width: 1280px;
+}
+
+.mt_20 {
+  margin-top: 20px;
+}
+
+.news_top > a {
+  width: 50%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 37.5%;
+}
+.news_top a {
+  float: left;
+}
+
+a:link,
+a:visited,
+a:active,
+a {
+  text-decoration: none;
+}
+.sub_content {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-bottom: 100px;
+}
+
+.mt_55 {
+  margin-top: 55px;
+}
+.news_cell {
+  width: 100%;
+  float: left;
+}
+.mt_15 {
+  margin-top: 15px;
+}
+
+.pt_20 {
+  padding-top: 20px;
+}
+.mb_100 {
+  margin-bottom: 100px;
+}
+.news_cell li {
+  float: left;
+  width: 49.1%;
+  height: 0;
+  padding-bottom: 26%;
+  margin-bottom: 27px;
+  overflow: hidden;
+  margin-right: 1.8%;
+  background: #fff;
+  border-bottom: 1px solid #eeeeee;
+}
+.page_area {
+  float: left;
+  width: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  text-align: center;
+}
+
+.fnt_16 {
+  font-size: 16px;
+}
+a.page_l_end {
+  background-image: url(../../../assets/l_off.png) no-repeat;
+}
+a.page_left {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: url(../../../assets/l_on.png) no-repeat;
+}
+.page_area a.page_num {
+  height: 35px;
+  padding: 0 3px 6px;
+  line-height: 35px;
+  margin: 0 1.5%;
+  position: relative;
+  top: -19px;
+}
+a.page_on {
+  color: #daaa38;
+  border-bottom: 2px solid #daaa38;
+}
+a.page_right {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background: url(../../../assets/r_on.png) no-repeat;
+}
 </style>
--- src/view/we/index.vue
+++ src/view/we/index.vue
@@ -1,6 +1,183 @@
 <template>
   <div id="we">
-      我们
+      <div class="sub_header_area">
+	<div class="header cf sub_h" style="top: 0px;">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<div class="header cf" style="top: 0px;">
+		<div class="logo" style="opacity: 1; top: 4px;"><a href="http://www.jxcc.com:80/"><img src="http://www.jxcc.com:80/static/img/sub_logo.png"></a></div>
+		<div class="nav_right" style="opacity: 1; right: 40px;">
+			<ul>
+					    <li>
+							<a href="http://www.jxcc.com:80/channel/4a864176f35f450689df46cbe6916cfb.html"><i>我们</i><span></span></a>
+							<dl style="display: none;">
+								<dd class=""><a href="http://www.jxcc.com:80/channel/748a941674804fbd864e321366debcf3.html" class="fnt_16">公司简介</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/4ccb06d3fa614beebeb737f89c09f998.html" class="fnt_16">发展历程</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/fd5aab01a1df42ca80541b9eb611eedb.html" class="fnt_16">组织架构</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/56545073c89342b1b58688b97b9a40a1.html" class="fnt_16">高管团队</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/43f0d4e0495d4de3b56c23eae5ccf4d2.html" class="fnt_16">我们公告</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/b4e6324bc52c40dd8fb15ff0234b468d.html" class="fnt_16">事业宣言</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/fbc9bc1708e44538a53dcb44d7b06ebb.html" class="fnt_16">江铜人</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/ed6933da35a94fe596f13c603fc6c88e.html" class="fnt_16">人才招聘</a></dd>
+							</dl>
+						</li>
+					    <li>
+							<a href="http://www.jxcc.com:80/channel/70490c55782a4e8b8e49dd724ada183a.html"><i>业务</i><span></span></a>
+							<dl style="display: none;">
+								<dd class=""><a href="http://www.jxcc.com:80/channel/7a20bb1593f34158aed8692df7e2d4e0.html" class="fnt_16">铜</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/e914aca95b3041d284fe6d2882d41e12.html" class="fnt_16">稀土</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/8709ca5870ff41d9b202b675ee07e735.html" class="fnt_16">金融</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/a3ffa84b7bd44118905c13dbb8f55192.html" class="fnt_16">贸易</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/6f9408a853d3404aaa862cc3a5383c69.html" class="fnt_16">铅锌</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/d6694dfad2734e0290e3d3d3abe0f1c4.html" class="fnt_16">黄金</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/8a1d7ba8fa2544138c3e952367e5769f.html" class="fnt_16">硫化工</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/5db552dc88f248ee8b03f804cceef4cc.html" class="fnt_16">物流</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/cb44343b04ff46caa1d2227128279632.html" class="fnt_16">技术服务</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/ee681ac1f5514363bed50f4cf6054603.html" class="fnt_16">稀散金属</a></dd>
+							</dl>
+						</li>
+					    <li>
+							<a href="http://www.jxcc.com:80/channel/274833fb8e304de1a34ce72886c40653.html"><i>投资者中心</i><span></span></a>
+							<dl style="display:none;">
+								<dd class=""><a href="http://www.jxcc.com:80/channel/0a475d36740e422a8f741724deda280d.html" class="fnt_16">公告</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/3a9427eb85d54f18b398a3abcce8751a.html" class="fnt_16">股票信息</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/e5f3577672da45dcb730b29a1c69522d.html" class="fnt_16">定期报告</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/e7dbc72739e249cb94d74da10ca4e46a.html" class="fnt_16">高管薪酬</a></dd>
+							</dl>
+						</li>
+					    <li>
+							<a href="http://www.jxcc.com:80/channel/a9a8b7f7352f4b2dad1f196c77f037dc.html"><i>可持续发展</i><span></span></a>
+							<dl style="display:none;">
+								<dd class=""><a href="http://www.jxcc.com:80/channel/9df3bd95738245e0a2dc664911e2e8bd.html" class="fnt_16">节能环保</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/27e8ca8666554beba16fe1fbffa82943.html" class="fnt_16">循环经济</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/02787afb4c974c7784ebc7aa099ac7a2.html" class="fnt_16">社会公益</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/945ed660fa2e4c76a2f9b77c359a9898.html" class="fnt_16">社会责任承诺</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/33e3059779324d29badd881e6696a033.html" class="fnt_16">技术需求</a></dd>
+							</dl>
+						</li>
+					    <li>
+							<a href="http://www.jxcc.com:80/channel/b9859596fca04cf99000ed8dd60e7e22.html"><i>党建创新</i><span></span></a>
+							<dl style="display:none;">
+								<dd class=""><a href="http://www.jxcc.com:80/channel/0dc5771f7807451b8b2bfb0264178872.html" class="fnt_16">党建动态</a></dd>
+								<dd class=""><a href="http://www.jxcc.com:80/channel/fc0bc5f64b0945538a09bbf30085cef3.html" class="fnt_16">党建特色</a></dd>
+							</dl>
+						</li>
+					   		<li>
+								<a href="http://www.jxcc.com:80/channel/d1d42c81a60e4838893cb20212c56a37.html"><i>传媒视点</i><span></span></a>
+								<dl style="display:none;">
+									<dd class=""><a href="http://www.jxcc.com:80/channel/6259e25f77314c4781f0bdd18c60a390.html" class="fnt_16">公司新闻</a></dd>
+									<dd class=""><a href="http://www.jxcc.com:80/channel/d1d42c81a60e4838893cb20212c56a37.html" class="fnt_16">媒体新闻</a></dd>
+								</dl>
+							</li>
+			</ul>
+			<a class="search_btn" href="http://www.jxcc.com:80/search/tosearchResult.html"></a>
+			<div class="language_sel"><a href="http://en.jxcc.com:80">English</a></div>
+		</div>
+		<div class="nav_btn"><span></span></div>
+	</div>
+<script type="text/javascript" src="http://www.jxcc.com:80/static/js/jquery.cookie.js"></script>
+<script type="text/javascript">
+var basehref="http://www.jxcc.com:80";
+function doPost(to, p) {
+    var myForm = document.createElement("form");
+    myForm.method = "post";  
+    myForm.action = to;
+    for (var i in p){
+       var myInput = document.createElement("input");       
+       myInput.setAttribute("name", i);  // 为input对象设置name  
+       myInput.setAttribute("value", p[i]);  // 为input对象设置value  
+       myForm.appendChild(myInput);  
+   }     
+   document.body.appendChild(myForm);     
+   myForm.submit();
+   document.body.removeChild(myForm);  // 提交后移除创建的form  
+}
+//切换多语言
+$(function(){
+	$(".language_sel").html("<a href=\"http://en.jxcc.com:80\">English</a>");
+	//$(".language_sel").html("<a href=\"http://en.jxcc.com:80/cmsbestJiangTong\">English</a>");
+});
+
+</script>
+
+	</div>
+</div>
+<div class="s_banner">
+	<!-- <img src="http://www.jxcc.com:80/static/img/gs_banner.jpg" /> -->
+               <div class="item">
+               			<img src="/uploadFiles/uploadImgs/20180105/5c2483df6e11431f8cde377b182a46cb.jpg" alt="">
+               </div>
+				<div class="banner_area w1280" style="width: 1280px;">
+					<span class="s_b_text">
+						<h1 class="fnt_30">我们</h1>
+						<h4 class="fnt_14">ABOUT</h4>
+					</span>
+				</div>
+</div>
+<div class="wrap_con_nobac pb_200">
+	<div class="sub_main_con cf">
+		<div class="sub_menu fnt_14">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<div class="sub_menu fnt_14">
+			<a href="http://www.jxcc.com:80/">首页</a>
+			<span></span>
+				    <a href="http://www.jxcc.com:80/channel/4a864176f35f450689df46cbe6916cfb.html">我们</a>
+		</div>		</div>
+	</div>
+	<div class="wrap_con_us">
+				<div class="sub_main_us cf min_h_us">
+					<div class="us_year fnt_60 mt_10">2019</div>
+					<a href="http://www.jxcc.com:80/channel/748a941674804fbd864e321366debcf3.html"><ul class="us_list"><li><p></p></li><li><p></p><h1 class="fnt_22">财富世界500强</h1><p></p><p class="fnt_18 mt_5">以348.7亿美元的营业收入位列《财富》世界500强第358位</p><p></p></li><li><p></p></li><li><p></p><h1 class="fnt_22">财富中国500强</h1><p></p><p class="fnt_18 mt_5">江西铜业以2152.9亿元的营业收入和24.47亿元的净利润位列榜单第45位</p><p class="fnt_18 mt_5">恒邦股份以212亿元的营业收入和4.06亿元的净利润位列榜单第393位</p><p></p></li><li><p></p></li><li><p></p><h1 class="fnt_22">《福布斯》全球企业2000强</h1><p></p><p class="fnt_18 mt_5">位居2019《福布斯》全球企业2000强第1227位</p><p></p></li><li><p></p></li><li><p></p><h1 class="fnt_22">中国企业500强</h1><p></p><p class="fnt_18 mt_5">以2306.54亿元的营业收入位居2019中国企业500强第88位</p><p></p></li></ul><p><br></p></a>
+				</div>
+	</div>
+		<div class="us_con_flist">
+			<ul>
+				<li>
+					<a href="http://www.jxcc.com:80/channel/fd5aab01a1df42ca80541b9eb611eedb.html">
+						<img src="http://www.jxcc.com:80/static/img/us_p01.jpg">
+						<h1 class="fnt_26 mt_25">组织架构</h1>
+					</a>
+				</li>
+				<li>
+					<a href="http://www.jxcc.com:80/channel/4ccb06d3fa614beebeb737f89c09f998.html">
+						<img src="http://www.jxcc.com:80/static/img/us_p02.jpg">
+						<h1 class="fnt_26 mt_25">发展历程</h1>
+					</a>
+				</li>
+				<li>
+					<a href="http://www.jxcc.com:80/channel/56545073c89342b1b58688b97b9a40a1.html">
+						<img src="http://www.jxcc.com:80/static/img/us_p03.jpg">
+						<h1 class="fnt_26 mt_25">高管团队</h1>
+					</a>
+				</li>
+				<li>
+					<a href="http://www.jxcc.com:80/channel/fbc9bc1708e44538a53dcb44d7b06ebb.html">
+						<img src="http://www.jxcc.com:80/static/img/us_p04.jpg">
+						<h1 class="fnt_26 mt_25">江铜人</h1>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="us_sy_con cf pt_100">
+			<div class="us_sy">
+										<div class="us_le_c" style="width: 665.6px; height: 665.6px;">
+											<a href="http://www.jxcc.com:80/channel/b4e6324bc52c40dd8fb15ff0234b468d.html">
+												<img src="/uploadFiles/uploadImgs/20180131/a5ee48403bbd4959b268ce5de4a3eef6.jpg">
+											</a>
+										</div>
+										<div class="us_ri_c" style="width: 665.6px; height: 665.6px; margin-top: 66.56px;">
+											<h1 class="fnt_36">事业宣言</h1>
+											<p>
+												身处新的发展阶段，立足新的发展起点，我们需要更加坚定的信念，来坚守内心深处的那份契约我们建立愿景，重构使命，是为了寻找更加清晰的激励和启示；
+											</p>
+											<p>
+										     我们反省责任与价值，是为了闪耀企业属性之中的人本光辉。 我们深知，文化是江铜事业的正向磁场江铜事业宣言，是江铜员工理想信念和精神气质的另一种表达。
+										    </p>
+										</div>
+			</div>
+		</div>
+</div>
   </div>
 </template>
 
@@ -11,5 +188,156 @@ export default {
 </script>
 
 <style>
+ .s_banner {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 20.9%;
+}
+.s_banner img {
+  width: 100%;
+}
+img {
+  -webkit-transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  -ms-transition: all 0.5s;
+  transition: all 0.5s;
+  border: 0;
+  display: inline-block;
+}
+.banner_area {
+  position: absolute;
+  max-width: 1280px;
+  left: 50%;
+  bottom: 10%;
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -o-transform: translateX(-50%);
+  -ms-transform: translateX(-50%);
+  transform: translateX(-50%);
+}
+.w1280 {
+  margin: 0 auto;
+  width: 1280px;
+}
+.s_b_text h1,
+.s_b_text h4 {
+  color: #fff;
+}
+.fnt_30 {
+  font-size: 30px;
+}
+.fnt_14 {
+    font-size: 14px;
+}
+meta {
+  display: none;
+}
+.wrap_con_nobac {
+    margin: 0 auto;
+    width: 100%;
+    background: none;
+}
 
+.pb_200 {
+    padding-bottom: 200px;
+}
+.sub_main_con {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 38px 0 0;
+}
+meta {
+    display: none;
+}
+.wrap_con_us {
+    margin: 0 auto;
+    background: url(../img/us_pics_n.jpg) no-repeat bottom center;
+    width: 100%;
+}
+.min_h_us {
+    min-height: 650px;
+}
+.sub_main_us {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 115px 0 0;
+}
+.us_year {
+    color: #dbc07f;
+    font-family: arial;
+}
+.fnt_60 {
+    font-size: 60px;
+}
+
+.mt_10 {
+    margin-top: 10px;
+}
+.us_list {
+    float: left;
+    width: 60%;
+    margin-top: 20px;
+}
+.us_con_flist {
+    margin: 0 auto;
+    background: #00638b;
+    height: 400px;
+    width: 100%;
+}
+.us_con_flist ul {
+    margin: 0 auto;
+    max-width: 1280px;
+    height: 100%;
+    background: url(../img/us_li_b.png) no-repeat right center;
+}
+.us_con_flist ul li {
+    float: left;
+    width: 25%;
+    height: 100%;
+    background: url(../img/us_li_b.png) no-repeat left center;
+    text-align: center;
+}
+.us_sy_con {
+    max-width: 1280px;
+    margin: 0 auto;
+}
+.pt_100 {
+    padding-top: 100px;
+}
+.us_sy {
+    position: relative;
+}
+.us_le_c {
+    position: absolute;
+    top: 0;
+    width: 50%;
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 0.1%;
+    left: 0;
+}
+.us_ri_c {
+    float: right;
+    width: 50%;
+    background: #f3f3f3;
+}
+.us_ri_c h1 {
+    margin-bottom: 20px;
+    width: 70%;
+    margin-left: 20%;
+    margin-top: 25%;
+}
+.fnt_36 {
+    font-size: 36px;
+}
+.us_ri_c p {
+    float: left;
+    margin-top: 20px;
+    font-size: 18px;
+    width: 70%;
+    margin-left: 20%;
+    color: #999;
+}
 </style>
