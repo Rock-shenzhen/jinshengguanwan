3309c8833963a6404c5063ceec4ccdf2d56bc398
zhuminghui <875939853@qq.com>
2020-04-23 18:17:41 +0800

    增加home,news，we，style中加了公共样式，还有assets中增加图片素材

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-23 17:38:55
+ * @LastEditTime: 2020-04-23 17:57:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \jsrl\jinshengguanwan\src\main.js
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -5,15 +13,16 @@ import App from './App'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
-
+import bass from './styles/base.css';
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    bass,
+    components: { App },
+    template: '<App/>'
 })
